e9aa04b0c760e4dda0a3bc265c5c7cd6f8e014dc
k4ps <konradschoen@gmx.de>
2019-02-09 13:01:09 +0100

    Init

---
+++ Computergrafic/bin/Debug/AppX/js/main.js
@@ -0,0 +1,34 @@
+﻿//new scene
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+//adding cube
+var geometry = new THREE.BoxGeometry(1, 1, 1);
+var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+var cube = new THREE.Mesh(geometry, material);
+
+
+function init() {
+    scene.add(cube);
+    camera.position.z = 5;
+}
+
+//render scene
+function animate() {
+    requestAnimationFrame(animate);
+    //animatied moving parts between here
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+    //and here
+    renderer.render(scene, camera);
+}
+animate();
+var main = function () {
+    this.init();
+    this.animate();
+}
+
+main();
---
+++ Computergrafic/js/main.js
@@ -0,0 +1,41 @@
+﻿//new scene
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+//adding cube
+var geometry = new THREE.BoxGeometry(1, 1, 1);
+var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+var cube = new THREE.Mesh(geometry, material);
+
+//adding table
+var geometry = new THREE.BoxGeometry(20, 1, 60, 1, 3, 1)
+var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+var tischplatte = new THREE.Mesh(geometry, material)
+
+
+function init() {
+    //scene.add(cube);
+    scene.add(tischplatte)
+    camera.position.z = 65;
+    camera.position.x = 50
+}
+
+//render scene
+function animate() {
+    requestAnimationFrame(animate);
+    //animatied moving parts between here
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+    //and here
+    renderer.render(scene, camera);
+}
+animate();
+var main = function () {
+    this.init();
+    this.animate();
+}
+
+main();
