d6cb40f43facabb64b28ffc66125ab375e243a46
k4ps <konradschoen@gmx.de>
2019-02-09 20:24:10 +0100

    Texturing to table, included gltfLoader, loaded two gltf

--- Computergrafic/js/main.js
+++ Computergrafic/js/main.js
@@ -5,6 +5,7 @@ init();
 // render();
 animate();
 
+
 function init() {
     // scene
     scene = new THREE.Scene();
@@ -27,6 +28,9 @@ function init() {
 
     // world
     create_table(0, 0, 0);
+    // create_dice_set(0, 10, 0);
+    load_dice_set();
+    load_dragon_statue();
 
     // lights
     var light = new THREE.DirectionalLight(0xffffff, 1);
@@ -48,88 +52,169 @@ function create_table(x_koord, y_koord, z_koord) {
     // Benoetigte Variablen
     var tischbreite = 112;
     var tischlaenge = 224;
-    var tischhoehe = 10;
-    var kantenhoehe = 5;
     var beinhoehe = 70;
-    var beinbreite = 10;
-
-    var kantenbreite = tischbreite / 8;
+    var beinbreite = tischbreite / 8;
+    var kantenbreite = beinbreite;
     var filzbreite = tischbreite - 2 * kantenbreite;
     var filzlaenge = tischlaenge - 2 * kantenbreite;
-    var oberflaeche = y_koord + 3 * tischhoehe / 4;
-    
 
-
-    // Materialien fuer den tisch
-    var texture_holz = new THREE.TextureLoader().load('images/holz.jpg');
-    texture_holz.wrapS = texture_holz.wrapT = THREE.MirroredRepeatWrapping;
-    texture_holz.repeat.set(2, 2)
-    var holz = new THREE.MeshLambertMaterial({ map: texture_holz });
-
-    var texture = new THREE.TextureLoader().load('image/filz.jpg');
-    // texture.wrapS = texture.wrapT = THREE.MirroredRepeatWrapping;
-    //texture.repeat.set(2, 2)
-    var filz = new THREE.MeshLambertMaterial({ map: texture });
+    var default_mat = new THREE.MeshLambertMaterial();
 
     // Tischplatte erstellen
-    var geometry = new THREE.BoxGeometry(tischbreite, tischhoehe, tischlaenge);   
-    var mesh = new THREE.Mesh(geometry, holz);
-    mesh.position.set(x_koord, y_koord, z_koord);
+    var geometry = new THREE.BoxGeometry(tischbreite - 2 * kantenbreite, kantenbreite / 2, tischlaenge - 2 * kantenbreite);
+    var texture = new THREE.TextureLoader().load('images/holz-vert.jpg');
+    texture.wrapS = texture.wrapT = THREE.MirroredRepeatWrapping;
+    texture.repeat.set(2, 3);
+    var material = new THREE.MeshLambertMaterial({ map: texture });
+
+    var mesh = new THREE.Mesh(geometry, material);
+    mesh.position.set(x_koord, y_koord - kantenbreite / 4, z_koord);
     scene.add(mesh);
 
     // Filzeinlage erstellen
     var geometry = new THREE.BoxGeometry(filzbreite, 0.1, filzlaenge);
+    var texture = new THREE.TextureLoader().load('images/filz.jpg');
+    texture.wrapS = texture.wrapT = THREE.MirroredRepeatWrapping;
+    texture.repeat.set(4, 20)
+    var filz = new THREE.MeshLambertMaterial({ map: texture });
     var mesh = new THREE.Mesh(geometry, filz);
-    mesh.position.set(x_koord, y_koord + tischhoehe / 2, z_koord);
+    mesh.position.set(x_koord, y_koord , z_koord);
     scene.add(mesh);
 
     // Die langen Kanten erstellen
-    var geometry = new THREE.BoxGeometry(kantenbreite, kantenhoehe, tischlaenge);
+    var geometry = new THREE.BoxGeometry(kantenbreite, kantenbreite, tischlaenge - 2* beinbreite);
+    var texture = new THREE.TextureLoader().load('images/holz-vert.jpg');
+    texture.wrapS = texture.wrapT = THREE.MirroredRepeatWrapping;
+    texture.repeat.set(1, 10);
+    var material = new THREE.MeshLambertMaterial({ map: texture });
 
     // links
-    var mesh = new THREE.Mesh(geometry, holz);
-    mesh.position.set(x_koord - tischbreite / 2 + kantenbreite/2, oberflaeche, z_koord);
+    var mesh = new THREE.Mesh(geometry, material);
+    mesh.position.set(x_koord - tischbreite / 2 + kantenbreite / 2, y_koord , z_koord);
     scene.add(mesh);
 
     // rechts
-    var mesh = new THREE.Mesh(geometry, holz);
-    mesh.position.set(x_koord + tischbreite / 2 - kantenbreite/2, oberflaeche, z_koord);
+    var mesh = new THREE.Mesh(geometry, material);
+    mesh.position.set(x_koord + tischbreite / 2 - kantenbreite / 2, y_koord , z_koord);
     scene.add(mesh);
 
     // Die kurzen Kanten erstellen
+    var geometry = new THREE.BoxGeometry(tischbreite - 2 * kantenbreite, kantenbreite, kantenbreite);
+    var texture = new THREE.TextureLoader().load('images/holz-vert.jpg');
+    texture.wrapS = texture.wrapT = THREE.MirroredRepeatWrapping;
+    texture.repeat.set(6, 2);
+    //texture.rotation.set(0.5);
+    var material = new THREE.MeshLambertMaterial({ map: texture });
+
     // vorne
-    var geometry = new THREE.BoxGeometry(tischbreite - 2* kantenbreite , kantenhoehe, kantenbreite);
-    var mesh = new THREE.Mesh(geometry, holz);
-    mesh.position.set(x_koord, oberflaeche, z_koord + tischlaenge / 2 - kantenbreite / 2);
+    var mesh = new THREE.Mesh(geometry, material);
+    mesh.position.set(x_koord, y_koord , z_koord + tischlaenge / 2 - kantenbreite / 2);
     scene.add(mesh);
 
     // hinten
-    var mesh = new THREE.Mesh(geometry, holz);
-    mesh.position.set(x_koord, oberflaeche, z_koord - tischlaenge / 2 + kantenbreite / 2);
-    scene.add(mesh);  
+    var mesh = new THREE.Mesh(geometry, material);
+    mesh.position.set(x_koord, y_koord , z_koord - tischlaenge / 2 + kantenbreite / 2);
+    scene.add(mesh);
 
     // Tischbeine
     var geometry = new THREE.BoxGeometry(beinbreite, beinhoehe, beinbreite);
 
+    var texture = new THREE.TextureLoader().load('images/holz-vert.jpg');
+    texture.wrapS = texture.wrapT = THREE.MirroredRepeatWrapping;
+    texture.repeat.set(1, 2);
+    var material = new THREE.MeshLambertMaterial({ map: texture });
+
     // vorne links
-    var mesh = new THREE.Mesh(geometry, holz);
-    mesh.position.set(x_koord - tischbreite / 2 + beinbreite / 2, y_koord - 0.5 * beinhoehe, z_koord + tischlaenge / 2 - beinbreite / 2);
+    var mesh = new THREE.Mesh(geometry, material);
+    mesh.position.set(x_koord - tischbreite / 2 + beinbreite / 2, y_koord  - 0.5 * (beinhoehe - kantenbreite), z_koord + tischlaenge / 2 - beinbreite / 2);
     scene.add(mesh);
 
     // hinten links
-    var mesh = new THREE.Mesh(geometry, holz);
-    mesh.position.set(x_koord - tischbreite / 2 + beinbreite / 2, y_koord - 0.5 * beinhoehe, z_koord - tischlaenge / 2 + beinbreite / 2);
+    var mesh = new THREE.Mesh(geometry, material);
+    mesh.position.set(x_koord - tischbreite / 2 + beinbreite / 2, y_koord  - 0.5 * (beinhoehe - kantenbreite), z_koord - tischlaenge / 2 + beinbreite / 2);
     scene.add(mesh);
 
     // hinten rechts
-    var mesh = new THREE.Mesh(geometry, holz);
-    mesh.position.set(x_koord + tischbreite / 2 - beinbreite / 2, y_koord - 0.5 * beinhoehe, z_koord - tischlaenge / 2 + beinbreite / 2);
+    var mesh = new THREE.Mesh(geometry, material);
+    mesh.position.set(x_koord + tischbreite / 2 - beinbreite / 2, y_koord  - 0.5 * (beinhoehe - kantenbreite), z_koord - tischlaenge / 2 + beinbreite / 2);
     scene.add(mesh);
 
     // vorne rechts
-    var mesh = new THREE.Mesh(geometry, holz);
-    mesh.position.set(x_koord + tischbreite / 2 - beinbreite / 2, y_koord - 0.5 * beinhoehe, z_koord + tischlaenge / 2 - beinbreite / 2);
+    var mesh = new THREE.Mesh(geometry, material);
+    mesh.position.set(x_koord + tischbreite / 2 - beinbreite / 2, y_koord - 0.5 * (beinhoehe - kantenbreite), z_koord + tischlaenge / 2 - beinbreite / 2);
     scene.add(mesh);
+}
+
+function create_dice_set(x_koord, y_koord, z_koord) {
+    var default_mat = new THREE.MeshLambertMaterial({ color: 0xffffff });
+
+    // d4
+    var geometry = new THREE.TetrahedronGeometry(0.75);
+    var mesh = new THREE.Mesh(geometry, default_mat);
+    mesh.position.set(x_koord + 1.5, y_koord + 0.5, z_koord - 1);
+    scene.add(mesh);
+
+    // d6
+    var geometry = new THREE.BoxGeometry(1, 1, 1);
+    var cube = new THREE.Mesh(geometry, default_mat);
+    cube.position.set(x_koord, y_koord + 0.5, z_koord);
+    scene.add(cube);
+
+    // d8
+    var geometry = new THREE.OctahedronGeometry(0.75);
+    var mesh = new THREE.Mesh(geometry, default_mat);
+    mesh.position.set(x_koord - 1.5, y_koord + 0.75, z_koord -1.5);
+    scene.add(mesh);
+    // d12
+    var geometry = new THREE.DodecahedronGeometry(0.75);
+    var mesh = new THREE.Mesh(geometry, default_mat);
+    mesh.position.set(x_koord - 1.5, y_koord + 0.75, z_koord + 1.5);
+    
+    scene.add(mesh);
+
+    // d20
+    var geometry = new THREE.IcosahedronGeometry(1);
+    var mesh = new THREE.Mesh(geometry, default_mat);
+    mesh.position.set(x_koord + 2, y_koord + 1, z_koord + 2);
+    mesh.rotation.z = THREE.Math.degToRad(72);
+    scene.add(mesh);
+}
+
+
+// Done by: Mathieu Vaillancourt 
+// Avaiable under: https://sketchfab.com/models/42e434900ee84225b576415d4d856bed
+function load_dice_set() {
+    var loader = new THREE.GLTFLoader();
+
+    loader.load('gltf/rpg dice set/scene.gltf', function (gltf) {
+
+        scene.add(gltf.scene);
+
+        gltf.animations; // Array<THREE.AnimationClip>
+        gltf.scene; // THREE.Scene
+        gltf.scenes; // Array<THREE.Scene>
+        gltf.cameras; // Array<THREE.Camera>
+        gltf.asset; // Object
+
+    });
+}
+
+// Done by: Ashraf Bouhadida 
+// Avaiable under: https://sketchfab.com/models/8d6c9e00b6234ae48a9ba8373d1a8b8f
+function load_dragon_statue() {
+    var loader = new THREE.GLTFLoader();
+
+    loader.load('gltf/dragon_decimated/scene.gltf', function (gltf) {
+
+        scene.add(gltf.scene);
+
+        gltf.animations; // Array<THREE.AnimationClip>
+        gltf.scene; // THREE.Scene
+        gltf.scenes; // Array<THREE.Scene>
+        gltf.cameras; // Array<THREE.Camera>
+        gltf.asset; // Object
+
+    });
 }
 
 //render scene
